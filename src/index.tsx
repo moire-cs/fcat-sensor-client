ee4665e55cc58a975eaa3e262f7e56be89a8c733
Bennett Hermanoff <bennett@hermanoff.dev>
2024-02-16 23:50:22 -0600

    feat: add react router

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+//route definitions
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
 
